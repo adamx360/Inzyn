12a4b3327ce9c6ad5a03454d16af6cb832c87c36
adamx360 <sliwax360@gmail.com>
2024-11-18 09:55:21 +0100

    button

--- app/src/main/java/com/example/inzyn/MainActivity.kt
+++ app/src/main/java/com/example/inzyn/MainActivity.kt
@@ -28,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
+        binding.clock.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+                .setAnchorView(R.id.clock).show()
         }
     }
 
